4f36f8308d1e158ec324d762e3c6dfd96cf685cd
ReactReduxDeveloper <cool.pomolov@ya.ru>
2022-07-02 23:38:28 +0300

    6_lessons_end

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,24 @@ import './App.css';
 
 function App() {
 
- return  <div>adadwdaw</div>
+ return  (
+     <div className="App">
+      <ul>
+       <li>css</li>
+       <li>html</li>
+       <li>react</li>
+       <li>JS</li>
+
+
+      </ul>
+
+
+
+     </div>
+
+
+
+ )
 
 
 
